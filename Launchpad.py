d3287adee2bf97bf4e5d9a8f96f9fc08c379cbf1
ArielleDong <55215559+ArielleDong@users.noreply.github.com>
2022-10-17 23:04:12 -0400

    Added comments to dataframe reader

--- Launchpad.py
+++ Launchpad.py
@@ -18,15 +18,18 @@ startYear = int(input())
 
 print("Input end year: ") 
 endYear = int(input())
-
+# loop for multiple inputs of countries from user
 for i in range(countryCount):
     print("Input country: ")
     countryTemp = input()
+
+    # Using pycountry to convert user input for country names into iso equivalents
     countryTemp = pycountry.countries.search_fuzzy(countryTemp)[0].alpha_3
     inputCountry.append(countryTemp)
 
 fig_size = (10, 5)
 f = plt.figure(figsize=fig_size)
+
 
 def TreeLossChart(df, startYear, endYear, inputCountry):
     #TODO: delete this block of code later, kept in case needed for referencing
@@ -42,13 +45,15 @@ def TreeLossChart(df, startYear, endYear, inputCountry):
     labels = []
     ax = f.add_subplot(1,1,1)
     for i in inputCountry: 
-
+        # iterates through the list of user-inputted countries, filters through the dataframe
+        # and creates a new dataframe based on user input for start/end year and country
         newdf2 = df.loc(axis = 0)[(df['iso'] == i) & (df['Year'] >= startYear) & (df['Year'] <= endYear)]
         """
         Remove this block later, kept in case needed
         newdf2 = newdf2.set_index(['Year'])
         newdf2 = newdf2.drop(['gfw_gross_emissions_co2e_all_gases__Mg'], axis=1)
         """
+        # plots each individual column on the same graph, with the label as the counttry name
         plt.plot(newdf2['Year'], newdf2['umd_tree_cover_loss__ha'], label = pycountry.countries.get(alpha_3=i).name)
         
 

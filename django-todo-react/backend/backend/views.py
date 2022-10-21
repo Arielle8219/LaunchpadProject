6e7c0e99ce2eace121dc5209dc3c1eb1c88415b6
ArielleDong <dong344@purdue.edu>
2022-10-21 16:49:12 -0400

    Started process of adding image data to html file to render website

--- django-todo-react/backend/backend/views.py
+++ django-todo-react/backend/backend/views.py
@@ -1,10 +1,18 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 from . import Launchpad
+import pandas as pd
 # Create your views here.
 def home(request):
+    
+    df = pd.read_csv('Tree cover loss in United States compared to other areas/treecover_loss_by_region__ha.csv')
+    # TODO: pass in image data to an html template
+    # take in user input? 
+    # also figure out how to convert image data into an actual image
+    # possibly relevant stack overflow: https://stackoverflow.com/questions/47263773/add-image-data-to-html-file
+    context['graph'] = Launchpad.TreeLossChart(df, 2004, 2020, ["USA"])
     return HttpResponse('<h1>Hello Django!</h1>')
-    # context['graph'] = TreeLossChart()
     # return render(request, 'x/dashboard.html', context)
 
 def about(request):

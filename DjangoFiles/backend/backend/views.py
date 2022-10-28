904cb73e19617f6ce20598890f76984d1470b412
ArielleDong <dong344@purdue.edu>
2022-10-28 17:21:28 -0400

    Added comments explaining new changes

--- DjangoFiles/backend/backend/views.py
+++ DjangoFiles/backend/backend/views.py
@@ -11,36 +11,43 @@ from backend.forms import InputForm
 import pycountry
 # Create your views here.
 def home(request):
-    
-    # TODO: take in user input for countries, start year, and end year
-    #countries = ['USA', 'DEU', 'GBR']
+
     if request.method == 'POST':
       # create an instance of our form, and fill it with the POST data
       # when the user inputs data into the form
+
       form = InputForm(request.POST)
+
+      # if the user inputs valid parameters:
       if form.is_valid():
         startYear = form.cleaned_data['startYear']
         endYear = form.cleaned_data['endYear']
         countriesRaw = form.cleaned_data['countries']
         countriesList = []
-        
-        # user input is somewhat working, but because of the way
-        # i've set up indexing, it never grabs the last value
 
+        # TODO: make getting user input for multiple countries more elegant
+        # and allow for greater variation in user input
+
+
+        # breaks up a large raw string of countries into smaller, individual ones
+        # divides them very primitively for now - slicing by commas
+        # then applies fuzzy search on individual countries
+
+        # side note - for forms, could probably create a new model for countries
+        # using ISO data - something to think about
         while (countriesRaw.find(',') != -1):
             countrySubstr = countriesRaw[0 : countriesRaw.find(',')]
             countriesRaw = countriesRaw[countriesRaw.find(',') + 1 :]
             countryTemp = pycountry.countries.search_fuzzy(countrySubstr)[0].alpha_3
             countriesList.append(countryTemp)
-        
+        countryTemp = pycountry.countries.search_fuzzy(countriesRaw)[0].alpha_3
+        countriesList.append(countryTemp)
         graphic = TreeLossChart(startYear, endYear, countriesList)
 
     else:
-        # this must be a GET request, so create an empty form
+        # GET request, so create an empty form with base values for the graphic
         form = InputForm()
         graphic = TreeLossChart(2000, 2020, [])
-    
-    
     
     # very simple html template with rendered graphic and input form
     return render(request, 'homepage.html', {'graphic':graphic, 'form': form})

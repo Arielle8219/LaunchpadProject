4eeb26d2fe8c8c45f4e924588058c87e07d100e0
ArielleDong <dong344@purdue.edu>
2022-10-29 18:50:59 -0400

    Using papaparse to read csv files for user input

--- DjangoFiles/backend/search-bar-tutorial/src/App.js
+++ DjangoFiles/backend/search-bar-tutorial/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Papa from "papaparse";
-
+import CSVFile from 'treecover_loss_by_region__ha.csv'
 
 // Allowed extensions for input file
 const allowedExtensions = ["csv"];
@@ -9,6 +9,7 @@ const allowedExtensions = ["csv"];
 function App() {
   return (
     <div className="App">
+      Papa.parse(CSVFile, config)
       <input placeholder="Enter Post Title"/>
     </div>
   );

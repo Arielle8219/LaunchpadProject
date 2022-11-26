c8e408dca32a732dc060f2d44b0e06606b1664e2
Arielle Dong <dong344@purdue.edu>
2022-11-25 19:59:24 -0800

    css styling changes

--- DjangoFiles/backend/backend/forms.py
+++ DjangoFiles/backend/backend/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 
+INTEGER_CHOICES= [tuple([x, x]) for x in range(2001, 2022)]
+
+
 class InputForm(forms.Form):
 
-   startYear = forms.IntegerField()
+   startYear = forms.IntegerField(label = "Start Year ", widget = forms.Select(choices = INTEGER_CHOICES))
 
-   endYear = forms.IntegerField()
+   endYear = forms.IntegerField(label = "End Year  ", widget = forms.Select(choices = INTEGER_CHOICES))
 
    countries = forms.CharField(max_length=100)
